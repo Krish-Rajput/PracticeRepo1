b4c46b0f346c15dede57d9d7b93c2a829cfb7853
Krish Rajput <0241csds060@niet.co.in>
2026-07-18 14:04:42 +0530

    Time: 6 ms (1.74%), Space: 45.1 MB (16.38%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> res=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        int i=0;
+        int dig=1;
+        int p=0;
+        while(i<target.length){
+            res.add("Push");
+            p++;
+            if(dig!=target[i]){
+                res.add("Pop");
+                p--;i--;
+            }i++;dig++;
+        }
+        return res;
+    }
+}
