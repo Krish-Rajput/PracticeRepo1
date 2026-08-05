df2d14a61b08af76a6282d455313e1f76213a933
Krish Rajput <0241csds060@niet.co.in>
2026-08-05 21:39:46 +0530

    Time: 35 ms (44.06%), Space: 46.8 MB (85.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
-                    res[0]=i;res[1]=j;
+                    res[0]=i;res[1]=j;break;
                 }
             }
         }return res;
