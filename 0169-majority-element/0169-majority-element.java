9af91d92fe467701adf0852b2afe2db6d05976ef
Krish Rajput <0241csds060@niet.co.in>
2026-08-28 21:00:44 +0530

    Time: 1 ms (99.89%), Space: 55.7 MB (31.89%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,13 +3,14 @@ class Solution {
         int ct=0;
         int cand=nums[0];
         for(int i=0;i<nums.length;i++){
-            if(ct==0){
-                cand=nums[i];
-            }
             if(nums[i]==cand){
                 ct++;
             }else{
                 ct--;
+            }
+            if(ct<=0){
+                cand=nums[i];
+                ct=1;
             }
         }return cand;
     }
